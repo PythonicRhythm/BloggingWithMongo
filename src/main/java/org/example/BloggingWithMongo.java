3807ca85462c2fefd314536229a2a00eaa5118da
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-06-05 19:16:52 -0500

    Initial commit

---
+++ src/main/java/org/example/BloggingWithMongo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.mongodb.client.MongoClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class BloggingWithMongo
+{
+    public static void main( String[] args )
+    {
+        MongoClient
+    }
+}
